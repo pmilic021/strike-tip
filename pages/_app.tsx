c0221e868e7b41b544ec00e293278d2cced31593
Josip Bojčić <jbojcic1@gmail.com>
2022-02-07 00:33:00 +0100

    add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,29 @@
-import '../styles/globals.scss';
+// import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import styles from '../styles/Home.module.scss';
+// import styles from '../styles/Home.module.scss';
 import Head from 'next/head';
 import { SettingsProvider } from '../lib/utils/settings';
 import { FirebaseProvider } from '../lib/data/firebase-context';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from 'styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SettingsProvider>
-      <FirebaseProvider>
-        <div className={styles.container}>
-          <Head>
-            <title>Stream Tip</title>
-            <meta name="description" content="Stream tipping widget" />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
+    <>
+      <Head>
+        <title>Stream Tip</title>
+        <meta name="description" content="Stream tipping widget" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-          <Component {...pageProps} />
-        </div>
-      </FirebaseProvider>
-    </SettingsProvider>
+      <ChakraProvider theme={theme}>
+        <SettingsProvider>
+          <FirebaseProvider>
+            <Component {...pageProps} />
+          </FirebaseProvider>
+        </SettingsProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
