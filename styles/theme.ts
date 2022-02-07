e0eb88e6c975bf708a5a85b3448c3ea72d1fc264
Akamitori <akamitori@tutanota.com>
2022-02-07 15:58:10 +0200

    Dark mode implemented

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,8 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
   components: {
     Input: {
       variants: {
