e0c220e6086377bff8da7d68178e0f89effe9f6a
Josip Bojčić <jbojcic1@gmail.com>
2022-02-07 14:15:04 +0100

    Add deployment info endpoint

--- pages/api/deployment-info.ts
+++ pages/api/deployment-info.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string;
+  targetedStrikeApi: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({ targetedStrikeApi: process.env.STRIKE_API_URL ?? '' });
 }
