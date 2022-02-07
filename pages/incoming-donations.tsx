b920071769d50c349f8d34f36f8fd25314b9564a
Petar Milic <pmilic@strike.me>
2022-02-07 03:04:10 +0100

    Finished incoming donation widget

--- pages/incoming-donations.tsx
+++ pages/incoming-donations.tsx
@@ -1,32 +1,47 @@
 import type { NextPage } from 'next';
 import { useSettingsContext } from '../lib/utils/settings';
 import useTipsSubscription from '../lib/data/use-tips-subscription';
-import { useCallback, useState } from 'react';
+import { ReactNode, useCallback } from 'react';
 import { DonationInfo } from '../lib/data/models';
+import { Center, useToast } from '@chakra-ui/react';
+import styles from '../styles/EmbeddableWidget.module.scss';
 
 const IncomingDonations: NextPage = () => {
   const { settings } = useSettingsContext();
-  const [lastTip, setLastTip] = useState<DonationInfo | undefined>();
+  const toast = useToast();
 
   const callback = useCallback(
     (tip) => {
-      console.log(tip);
-      setLastTip(tip);
+      toast({
+        position: 'bottom',
+        render: donationSplash(tip),
+      });
     },
-    [setLastTip]
+    [toast]
   );
 
   useTipsSubscription(settings.username, callback);
 
-  return (
-    <main>
-      <div>{JSON.stringify(settings)}</div>
-      <section>
-        <label>Incoming donations widget</label>
-        {lastTip && <div>{JSON.stringify(lastTip)}</div>}
-      </section>
-    </main>
-  );
+  return null;
 };
+
+function donationSplash(donation: DonationInfo): () => ReactNode {
+  const render = () => (
+    <Center
+      p={3}
+      flexDirection="column"
+      justifyContent="stretch"
+      className={styles.incomingDonation}
+    >
+      <span>
+        {donation.donor}{' '}
+        <span className={styles.amount}>{donation.amount}$</span>
+      </span>
+      <span>{donation.message}</span>
+    </Center>
+  );
+
+  return render;
+}
 
 export default IncomingDonations;
