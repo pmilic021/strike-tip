c19964405e1cd0c67161dd71e53db49a61a13d90
Akamitori <akamitori@tutanota.com>
2022-02-07 05:17:10 +0200

    Implemented copy to clipboard+ fixed the campaign settings page to be less ugly

---
+++ components/linkItem.js
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import {
+  Button,
+  Text,
+  HStack,
+  Box,
+  Link,
+  useClipboard,
+} from '@chakra-ui/react';
+import { CopyIcon, CheckIcon } from '@chakra-ui/icons';
+import NextLink from 'next/link';
+import { useCallback, useState } from 'react';
+
+export default function LinkItem({ header, pathname, query, absPath, image }) {
+  const [value, setValue] = useState(absPath);
+  const { hasCopied, onCopy } = useClipboard(value);
+
+  return (
+    <HStack spacing="14px">
+      <Box>
+        <NextLink
+          href={{
+            pathname: pathname,
+            query: query,
+          }}
+        >
+          <Link>
+            <Image src={`/images/${image}`} height={65} width={65} />
+          </Link>
+        </NextLink>
+      </Box>
+      <Box>
+        <Text fontSize="3xl">{header}</Text>
+      </Box>
+      <Box>
+        <Button height={65} width={65} onClick={onCopy}>
+          {hasCopied ? <CheckIcon /> : <CopyIcon />}{' '}
+        </Button>
+      </Box>
+    </HStack>
+  );
+}
